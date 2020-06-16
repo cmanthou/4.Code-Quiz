d1ed58df61095797414a6b026aae57a969648fa9
cmanthou <cory.manthou@gmail.com>
2020-06-15 22:20:39 -0400

    Fixed JS

--- assets/js/assets.js
+++ assets/js/assets.js
@@ -2,27 +2,27 @@ var questions = [
     {
       title: "Commonly used data types DO NOT include:",
       choices: [" Strings", " Booleans", " Alerts", " Numbers"],
-      answer: "Alerts"
+      answer: " Alerts"
     },
     {
       title: "The condition in an if / else statement is enclosed within ____ .",
       choices: [" Quotes", " Curly Brackets", " Parentheses", " Square Brackets"],
-      answer: "Parentheses"
+      answer: " Parentheses"
     },
     {
       title: "Which of these is not used to loop?",
       choices: [" For", " While", " Foreach", " Sequence"],
-      answer: "Sequence"
+      answer: " Sequence"
     },
     {
       title: "Which of these is not a way to save a variable?",
       choices: [" Vet", " Var", " Let", " Const"],
-      answer: "Vet"
+      answer: " Vet"
     },
     {
       title: "JS date function starts in seconds to current day from what day in 1970?",
       choices: [" January 1", " December 31", " June 1", " April 23"],
-      answer: "January 1"
+      answer: " January 1"
     },
   
   ];
